8bfc9a3756389ce34fd93a886c4c43c231bf0031
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2023-12-03 22:28:54 -0500

    Update server1.rs

--- PaymentSplittingApp/src/bin/server1.rs
+++ PaymentSplittingApp/src/bin/server1.rs
@@ -188,6 +188,7 @@ fn handle_client(mut stream: TcpStream, issuer: Issuer, counter: Arc<Mutex<usize
             }
             let encoded = bincode::serialize(&success).unwrap();
             let _ = stream.write(&encoded);
+            println!("All Done!");
         }
         // TYPE: SETTLING
         // DATA: Settle Request
