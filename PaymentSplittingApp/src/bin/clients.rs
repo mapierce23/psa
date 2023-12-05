5e9e82e60f0ee38608b25559c0b3a5b219a46226
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2023-12-05 14:55:43 -0500

    Update clients.rs

--- PaymentSplittingApp/src/bin/clients.rs
+++ PaymentSplittingApp/src/bin/clients.rs
@@ -100,7 +100,7 @@ fn setup_group(group_size: usize) -> Result<Vec<GroupTokenPriv>, std::io::Error>
         match now.elapsed() {
             Ok(elapsed) => {
                 // it prints '2'
-                println!("{}", elapsed.as_nanos());
+                println!("{}", elapsed.as_nanos() as f64 / (1000000000 as f64));
             }
             Err(e) => {
                 // an error occurred!
