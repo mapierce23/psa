d06f759b6d452ea343dea87962f8cef44e91fa06
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2023-12-06 13:35:57 -0500

    Update server2.rs

--- PaymentSplittingApp/src/bin/server2.rs
+++ PaymentSplittingApp/src/bin/server2.rs
@@ -45,7 +45,7 @@ fn handle_client(mut stream: TcpStream, counter: Arc<Mutex<usize>>, database: Ar
     for _ in 0..1000 {
 
         // Remaining bytes is the type of request & the request itself
-        let mut buf = [0u8;10192];
+        let buf = &mut Vec<u8>;
         let bytes_read = stream.read_to_end(&mut buf)?;
 
         if bytes_read == 0 {
