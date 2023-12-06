0ea2ee321d3df77cfba56c5161a295948f086e64
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2023-12-06 10:50:18 -0500

    Update sketch.rs

--- PaymentSplittingApp/src/sketch.rs
+++ PaymentSplittingApp/src/sketch.rs
@@ -148,8 +148,6 @@ where
     pub fn sketch_at(
         &self,
         vector_in: &[(T, T)],
-        val_share: T,
-        val2_share: T,
         rand_stream: &mut impl rand::Rng,
     ) -> SketchOutput<T> {
         let mut out: SketchOutput<T> = SketchOutput::zero();
