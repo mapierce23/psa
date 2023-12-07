213d111d3144bd29b55382d38bc6af2de0d4cbfc
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2023-12-06 19:35:49 -0500

    Update server1.rs

--- PaymentSplittingApp/src/bin/server1.rs
+++ PaymentSplittingApp/src/bin/server1.rs
@@ -124,7 +124,7 @@ fn handle_client(mut stream: TcpStream, issuer: Issuer, counter: Arc<Mutex<usize
             let (com_x, com_ix, g_r2, g_r3) = compute_coms_from_dpf(&eval_all_src, td.r2, td.r3); // Four Ristrettos (compressed)
             let w1 = same_group_val_compute(&eval_all_src, &eval_all_dest, true);
             let now_d = SystemTime::now();
-            let mur prg: ChaCha8Rng = ChaCha8Rng::seed_from_u64((td.id as u64) + 56789u64);
+            let mut prg: ChaCha8Rng = ChaCha8Rng::seed_from_u64((td.id as u64) + 56789u64);
             let zero_bytes = [0u8; 16];
             let mut rvec = Vec::<FieldElm>::new();
             // Compute inner product 
