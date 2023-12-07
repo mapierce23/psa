162da0c5c66615ddaaa80ec8580a9cb2be220333
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2023-12-06 19:36:34 -0500

    Update server2.rs

--- PaymentSplittingApp/src/bin/server2.rs
+++ PaymentSplittingApp/src/bin/server2.rs
@@ -77,8 +77,23 @@ fn handle_client(mut stream: TcpStream, counter: Arc<Mutex<usize>>, database: Ar
             // ===========================================================================
             let (com_x, com_ix, g_r2, g_r3) = compute_coms_from_dpf(&eval_all_src, td.r2, td.r3); // Four Ristrettos (compressed)
             let w1 = same_group_val_compute(&eval_all_src, &eval_all_dest, false);
+            let mut prg: ChaCha8Rng = ChaCha8Rng::seed_from_u64((td.id as u64) + 56789u64);
+            let zero_bytes = [0u8; 16];
+            let mut rvec = Vec::<FieldElm>::new();
+            // Compute inner product 
+            let mut prod = FieldElm::one();
+            for i in 0..MAX_GROUP_SIZE {
+                let mut buf = [0u8; 16];
+                prg.fill_bytes(&mut buf);
+                let mut output = buf.to_vec();
+                output.extend(zero_bytes.clone());
+                let scalar = Scalar::from_bytes_mod_order(output.try_into().unwrap());
+                rvec.push(FieldElm {value : scalar});
+                prod.mul(&w1[i]);
+                prod.mul(&rvec[i]);
+            }
             let mut hasher = Sha256::new();
-            hasher.update(bincode::serialize(&w1).unwrap());
+            hasher.update(bincode::serialize(&prod).unwrap());
             let result = hasher.finalize();
             // let corshare_2 = sketch_at(); // TODO. Result of 1st step of sketching protocol
             let package = TransactionPackage {
