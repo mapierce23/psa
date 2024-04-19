4712c008959e0eb6380af222af71a0c8de74bbe2
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2024-04-19 15:59:50 -0400

    Update server2.rs

--- PaymentSplittingApp/src/bin/server2.rs
+++ PaymentSplittingApp/src/bin/server2.rs
@@ -57,8 +57,8 @@ fn handle_client(mut stream: TcpStream, counter: Arc<Mutex<usize>>, database: Ar
         // TYPE: TRANSACTION
         // DATA: TransactionData struct
         if buf[0] == 4 {
-            let bytes_read = 2610;
-            let mut buf1 = vec![0;2610];
+            let bytes_read = 2572;
+            let mut buf1 = vec![0;2572];
             stream.read_exact(&mut buf1)?;
             let res = bincode::deserialize(&buf1[0..bytes_read]);
             if res.is_err() {
