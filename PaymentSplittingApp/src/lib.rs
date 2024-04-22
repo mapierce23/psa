a35b991f416223b3af6124c380384bacdea904bf
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2024-04-22 00:19:38 -0400

    Update lib.rs

--- PaymentSplittingApp/src/lib.rs
+++ PaymentSplittingApp/src/lib.rs
@@ -17,10 +17,10 @@ pub use crate::field::FieldElm;
 //pub use crate::rpc::CollectorClient;
 
 pub const MAX_GROUP_SIZE: usize = 30;
-pub const MAX_GROUP_NUM: usize = 546;
-pub const DPF_DOMAIN: usize = 15; // 9 = 2^8 = 256 // about 60 AES evals
-pub const SETTLE_DOMAIN: usize = 11; // 9 = 2^8 = 256 // about 60 AES evals
-pub const SETTLE_SIZE: usize = 290;
+pub const MAX_GROUP_NUM: usize = 2184;
+pub const DPF_DOMAIN: usize = 17; // 9 = 2^8 = 256 // about 60 AES evals
+pub const SETTLE_DOMAIN: usize = 13; // 9 = 2^8 = 256 // about 60 AES evals
+pub const SETTLE_SIZE: usize = 328;
 
 // Additive group, such as (Z_n, +)
 pub trait Group {
