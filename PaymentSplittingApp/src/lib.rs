c4b5721480264b416ed942052a0c810836296e81
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2024-04-22 11:12:05 -0400

    Update lib.rs

--- PaymentSplittingApp/src/lib.rs
+++ PaymentSplittingApp/src/lib.rs
@@ -16,9 +16,9 @@ extern crate lazy_static;
 pub use crate::field::FieldElm;
 //pub use crate::rpc::CollectorClient;
 
-pub const MAX_GROUP_SIZE: usize = 30;
-pub const MAX_GROUP_NUM: usize = 4369;
-pub const DPF_DOMAIN: usize = 18; // 9 = 2^8 = 256 // about 60 AES evals
+pub const MAX_GROUP_SIZE: usize = 10;
+pub const MAX_GROUP_NUM: usize = 100;
+pub const DPF_DOMAIN: usize = 11; // 9 = 2^8 = 256 // about 60 AES evals
 pub const SETTLE_DOMAIN: usize = 15; // 9 = 2^8 = 256 // about 60 AES evals
 pub const SETTLE_SIZE: usize = 366;
 
